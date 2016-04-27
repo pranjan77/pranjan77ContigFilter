4143c1b22788d02ee664272931be088ba94063b8
Priya Ranjan <pranjan77@gmail.com>
2016-04-27 13:44:41 -0400

    initial commit

---
+++ lib/pranjan77ContigFilter/pranjan77ContigFilterImpl.py
@@ -0,0 +1,28 @@
+#BEGIN_HEADER
+#END_HEADER
+
+
+class pranjan77ContigFilter:
+    '''
+    Module Name:
+    pranjan77ContigFilter
+
+    Module Description:
+    A KBase module: pranjan77ContigFilter
+    '''
+
+    ######## WARNING FOR GEVENT USERS #######
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    #########################################
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        #END_CONSTRUCTOR
+        pass
---
+++ pranjan77ContigFilter.spec
@@ -0,0 +1,9 @@
+/*
+A KBase module: pranjan77ContigFilter
+*/
+
+module pranjan77ContigFilter {
+    /*
+        Insert your typespec information here.
+    */
+};
